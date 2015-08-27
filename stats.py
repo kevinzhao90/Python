c7c39d33d3bcc448a297148d7c288ddffb7e134a
Kevin Zhao <kevinzhaozifeng@gmail.com>
2015-08-27 14:28:56 -0500

    first final version

--- stats.py
+++ stats.py
@@ -4,7 +4,15 @@ def mean(vals):
 	return float(total)/length
 
 def std(vals):
-	if len(vals) == 0:
+	n = len(vals)
+	if n== 0:
 		return 0.0
-	return vals[-1]/2.0
+	mu = sum(vals)/n
+	var = 0.0
+	for val in vals:
+		var = var + (val-mu)**2
+	return (var/n)**0.5
+
+
+		
 
--- test_stats.py
+++ test_stats.py
@@ -29,3 +29,14 @@ def test_std3():
 	assert_equal(obs, exp)
 
 
+def test_std4():
+	obs = std([1.0, 3.0])
+	exp = 1.0
+	assert_equal(obs, exp)
+
+def test_std5():
+	obs =std([1.0, 1.0, 1.0])
+	exp = 0.0
+	assert_equal(obs, exp)
+
+
